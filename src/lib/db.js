b53185347eb0686eaf268a114ef27773643a63e5
Rodrigo <Lozano0309@gmail.com>
2025-01-06 15:45:44 -0400

    guardado

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Miguelangelomy1',
+  password: '',
   database: 'empresa',
   waitForConnections: true,
   connectionLimit: 10,
