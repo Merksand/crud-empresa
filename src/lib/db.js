2887bbfa3c666ce85af562f6af3e123772545c1b
Merksand <miguelangelsp7161@gmail.com>
2025-02-08 10:09:11 -0400

    del db

--- src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-import 'dotenv/config';
-
-export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'empresa',
-});
