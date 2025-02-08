d7444e30035fe346c273ac4cbb267b28a0736a88
Merksand <miguelangelsp7161@gmail.com>
2025-02-08 10:11:50 -0400

    add bd

---
+++ src/lib/db.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+
+export const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password:  '',
+  database: 'empresa',
+});
+
